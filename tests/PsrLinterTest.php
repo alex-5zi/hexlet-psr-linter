2832ea5e17175ca34625922146c9ca046c9e3632
Aleksandr <alex-5zi@rambler.ru>
2016-08-09 10:42:01 +0300

    test step2 3

--- tests/PsrLinterTest.php
+++ tests/PsrLinterTest.php
@@ -10,7 +10,8 @@ class PsrLinterTest extends \PHPUnit_Framework_TestCase
     public function testGetLog()
     {
         $root = vfsStream::setup('home');
-        vfsStream::newFile('test.php')->at($root)->setContent('<?php class PsrLinterTest { public function DconsDtruct(){}}');
+        vfsStream::newFile('test.php')->at($root)
+                                      ->setContent('<?php class PsrLinterTest { public function DconsDtruct(){}}');
         $psrlint = new PsrLinter();
         $psrlint->addFile(vfsStream::url('home/test.php'));
         $log = $psrlint->run()->getLog();
